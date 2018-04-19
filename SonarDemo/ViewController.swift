1d4c6eb539fa3bd79511a0d50db9a48564d04187
Dev Jangir <devpassion7@gmail.com>
2018-04-19 14:44:15 +0530

    Base Project

---
+++ SonarDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SonarDemo
+//
+//  Created by Devdutt Jangir on 19/04/18.
+//  Copyright © 2018 Devdutt Jangir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
